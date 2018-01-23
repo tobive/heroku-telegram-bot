41a97e746b8fd7a6a7600b40131dd64bb7244c6a
tobive <knightsofkangouw@gmail.com>
2018-01-23 22:08:21 +0700

    add answer's format

--- api_handler.py
+++ api_handler.py
@@ -1,20 +1,29 @@
 import requests
 
 class ApiHandler:
-    """parse given message and then request to Crypto API.
+    """Parse given message and then request to Crypto API.
         Send back response from Crypto API.
     """
     def __init__(self):
         self.api_url = 'https://api.cryptonator.com/api/full/'
 
     def get_response(self, text):
-        """parse text, and return appropriate response."""
+        """Parse text, and return appropriate response."""
         if '-' in text:
-            res = self.request_api(text)
+            args = text.split(' ')
+            if len(args) > 2:
+                return 'Unrecognized command: <{}> Please see /help'.format(text)
+            pair = args[0]
+            res = self.request_api(pair)
             if res['success'] == True:
-                return res['ticker']['price']
+                market = args[1] if len(args) == 2 else None
+                return format_answer(res, market)
+                # return self.format_currency(res['ticker']['price'])
             else:
-                return 'Sorry, request failed. Please try again later.'
+                if res['error'] == 'Pair not found':
+                    return 'Pair <{}> not found. Please try another pair'.format(text)
+                else:
+                    return 'Sorry, request failed. Please try again later.'
         else:
             return 'Unrecognized command: <{}> Please try again'.format(text)
 
@@ -23,3 +32,27 @@ class ApiHandler:
         res = requests.get(self.api_url + req)
         print("getting response from crypto api...")
         return res.json()
+
+    def format_currency(self, amount, offset=False):
+        """"Return string of amount in currency format"""
+        return '{:20,}'.format(float(amount))
+
+    def format_answer(self, res, market):
+        """Accept json from crypto API's response. Format answer and return as string."""
+        available = "No specific market is available.\n"
+        price = format_currency(res['ticker']['price'])
+        vol = format_currency(res['ticker']['volume'])
+        for obj in res['ticker']['markets']:
+            if obj['market'].lower() == market.lower():
+                price = format_currency(obj['price'])
+                vol = format_currency(obj['volume'])
+
+        if market is not None:
+            available = "Price of {base} in {market} Market\n".format(base=res['ticker']['base'], market)
+
+        answer = """{available}
+            Price : {price}\n
+            Volume : {volume}\n
+            """.format(available=available, price=price, volume=vol)
+
+        return answer
--- main.py
+++ main.py
@@ -57,9 +57,9 @@ def main():
     dp.add_error_handler(error)
 
     # Start the Bot
-    # updater.start_polling()
-    updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
-    updater.bot.set_webhook("https://tobi-telegram-bot.herokuapp.com/" + TOKEN)
+    updater.start_polling()
+    # updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
+    # updater.bot.set_webhook("https://tobi-telegram-bot.herokuapp.com/" + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
